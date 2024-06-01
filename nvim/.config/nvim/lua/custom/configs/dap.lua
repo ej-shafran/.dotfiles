1d51e700651404109e17aff899f3614e439945cf
ejshafran <ejshafran@hilma.tech>
2024-06-02 02:05:24 +0300

    Add Neovim debugging (C)

---
+++ nvim/.config/nvim/lua/custom/configs/dap.lua
@@ -0,0 +1,57 @@
+local dap = require "dap"
+local dapui = require "dapui"
+local virtual_text = require "nvim-dap-virtual-text"
+
+dapui.setup()
+virtual_text.setup {}
+
+dap.adapters.gdb = {
+  id = "gdb",
+  type = "executable",
+  command = "gdb",
+  args = { "--quiet", "--interpreter=dap" },
+}
+
+dap.configurations.c = {
+  {
+    name = "Run executable (GDB)",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Run executable with arguments (GDB)",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    args = function()
+      return vim.split(vim.fn.input "Arguments: ", " +")
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Attach to process (GDB)",
+    type = "gdb",
+    request = "attach",
+    pid = require("dap.utils").pick_process,
+  },
+}
+dap.configurations.zig = dap.configurations.c
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
---
+++ nvim/.config/nvim/lua/custom/plugins/debugging.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require "custom.configs.dap"
+    end,
+  },
+}
