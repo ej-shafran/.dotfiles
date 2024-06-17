7d5f2263491f5f1129bc72fe3f40a68ba63a5191
ej-shafran <ejshafran@hilma.tech>
2024-06-17 23:45:44 +0300

    [emacs] Some packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(blamer cmake-mode which-key use-package magit ivy gnu-elpa-keyring-update git-gutter-fringe evil-collection company)))
+   '(lua-mode cmake-mode zen-mode which-key use-package magit ivy gnu-elpa-keyring-update git-gutter-fringe evil-collection company blamer)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
