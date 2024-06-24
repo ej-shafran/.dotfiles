92b39fcc1496678f29546d604de67b5b63d5c11e
ejshafran <ejshafran@hilma.tech>
2024-06-24 14:47:41 +0300

    Add executable checks

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if ! command -v nix >/dev/null 2>&1; then
+    echo "Missing \`nix\` executable; make sure to have it installed!" >&2
+    exit 1
+fi
+
+if [ "$#" -gt 0 ] && [ "$1" != remove ]; then
+    echo "Usage: ./dependencies.sh [remove]" >&2
+    echo "  Install or remove all dependencies for this system." >&2
+    echo "  Run with \`remove\` to uninstall the dependencies." >&2
+    exit 1
+fi
+
 currentSystem="$(nix eval --impure --raw --expr 'builtins.currentSystem')"
 
 if [ "$1" != remove ]; then
--- packages.sh
+++ packages.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if ! command -v stow >/dev/null 2>&1; then
+    echo "Missing \`stow\` executable; make sure to have it installed!" >&2
+    exit 1
+fi
+
+if [ "$#" -gt 0 ] && [ "$1" != remove ]; then
+    echo "Usage: ./packages.sh [remove]" >&2
+    echo "  Install or remove all \`stow\` packages in this directory." >&2
+    echo "  Run with \`remove\` to uninstall the packages." >&2
+    exit 1
+fi
+
 for file in *; do
     if [ -d "$file" ]; then
         if [ "$1" != remove ]; then
