93ef54d6fec458f6f53aedfb4bc95c4a30cb2a0c
ejshafran <ejshafran@hilma.tech>
2024-07-04 14:57:38 +0300

    [emacs] Add some packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -64,6 +64,21 @@
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
+;; Git gutter
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02)
+  )
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
+;; Git blame on lines
+(use-package blamer)
+
 ;; Theme
 (use-package gruber-darker-theme
   :config
