7f074bd5b5107ef6993c55a7b73468d6f00a9663
ej-shafran <ejshafran@hilma.tech>
2024-07-09 15:50:00 +0300

    [nvim] Update lua snippets

--- nvim/.config/nvim/lua/custom/snippets/lua.lua
+++ nvim/.config/nvim/lua/custom/snippets/lua.lua
@@ -2,8 +2,9 @@ require("luasnip.session.snippet_collection").clear_snippets "lua"
 
 local ls = require "luasnip"
 local s = ls.snippet
-local fmt = require("luasnip.extras.fmt").fmt
+local f = ls.function_node
 local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("lua", {
   s(
@@ -17,6 +18,24 @@ ls.add_snippets("lua", {
       return M
       ]],
       i(0)
+    )
+  ),
+  s(
+    "nmod",
+    fmt(
+      [[
+      local {name} = {{}}
+
+      {}
+
+      return {name}
+      ]],
+      {
+        i(0),
+        name = f(function()
+          return string.lower(vim.fn.expand "%:t:r")
+        end),
+      }
     )
   ),
 })
