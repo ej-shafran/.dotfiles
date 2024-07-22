6ceb764359c0dc5bc584e70671cebfebb91adf58
ejshafran <ejshafran@hilma.tech>
2024-07-22 12:02:06 +0300

    [emacs] Custom file

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ivy lsp-mode typescript-mode which-key spinner multiple-cursors markdown-mode magit lv lua-mode ht gruber-darker-theme gnu-elpa-keyring-update git-gutter-fringe f evil-collection counsel company clang-format blamer)))
+   '(lsp-ivy lsp-mode which-key typescript-mode spinner multiple-cursors markdown-mode magit lv lua-mode ht gruber-darker-theme gnu-elpa-keyring-update git-gutter-fringe f evil-collection counsel company clang-format blamer)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
