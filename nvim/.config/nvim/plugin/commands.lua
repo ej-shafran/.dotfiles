e1da8734daa0272e022ade4646486180b0bda7be
ejshafran <ejshafran@hilma.tech>
2024-08-24 22:18:57 +0300

    [nvim] Add highlight debugging

--- nvim/.config/nvim/plugin/commands.lua
+++ nvim/.config/nvim/plugin/commands.lua
@@ -2,3 +2,4 @@ local command = vim.api.nvim_create_user_command
 
 command("LazyReloadPlugin", require("custom.pickers.lazy_reload_plugin").run, {})
 command("RightAlign", "normal mc<count>A <esc>0<count>lDgelD`cP", { count = 80 })
+command("DebugHighlights", require("custom.commands.debug_highlights").toggle, {})
