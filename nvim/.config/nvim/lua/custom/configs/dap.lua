9a750e2034a8ced944c5c5aac569f5c952a0e779
ejshafran <ejshafran@hilma.tech>
2025-02-05 01:31:08 +0200

    [nvim] Add lldb-dap

--- nvim/.config/nvim/lua/custom/configs/dap.lua
+++ nvim/.config/nvim/lua/custom/configs/dap.lua
@@ -5,6 +5,13 @@ local virtual_text = require "nvim-dap-virtual-text"
 dapui.setup()
 virtual_text.setup {}
 
+dap.adapters.lldb = {
+  id = "lldb",
+  type = "executable",
+  command = "lldb-dap",
+  args = {},
+}
+
 dap.adapters.gdb = {
   id = "gdb",
   type = "executable",
@@ -37,6 +44,33 @@ dap.configurations.c = {
   {
     name = "Attach to process (GDB)",
     type = "gdb",
+    request = "attach",
+    pid = require("dap.utils").pick_process,
+  },
+  {
+    name = "Run executable (LLDB)",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Run executable with arguments (LLDB)",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    args = function()
+      return vim.split(vim.fn.input "Arguments: ", " +")
+    end,
+    cwd = "${workspaceFolder}",
+  },
+  {
+    name = "Attach to process (LLDB)",
+    type = "lldb",
     request = "attach",
     pid = require("dap.utils").pick_process,
   },
